daf9f810c4ab53ec5ffe9756dacfce7b05c88f4a
Ralph Wen <ralph.wen@gmail.com>
2014-07-20 21:18:07 +0800

    restructing

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'yeah this is a key'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASK_BLOG_SUBJECT_PREFIX = '[Test flask blog]'
+    FLASK_BLOG_MAIL_SENDER = 'Ralph Wen <ralph.wen@gmail.com>'
+    FLASK_BLOG_ADMIN = os.environ.get('FLASK_BLOG_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') \
+        or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    Testing = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') \
+        or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') \
+        or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
