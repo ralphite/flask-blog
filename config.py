45e93cd3a072a82f4588951940af60a93165d1c9
Ralph Wen <ralph.wen@gmail.com>
2014-07-21 21:31:56 +0800

    fix bugs in manager etc

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'ralph.wen@gmail.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASK_BLOG_MAIL_SUBJECT_PREFIX = '[Test flask blog]'
+    FLASK_BLOG_MAIL_SUBJECT_PREFIX = '[Flask Blog]'
     FLASK_BLOG_MAIL_SENDER = 'Ralph Wen <ralph.wen@gmail.com>'
     FLASK_BLOG_ADMIN = os.environ.get('FLASK_BLOG_ADMIN') or 'ralph.wen@gmail.com'
 
