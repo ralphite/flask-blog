cd74393708d77bb394a29905c2b1f5e7406890e9
Ralph Wen <ralph.wen@gmail.com>
2014-07-23 11:33:38 +0800

    add edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import Form
 
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(Form):
     name = StringField("Your name?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
