306f92d340b7f02d1a4c76f0c2b569222fbc4e57
Ralph Wen <ralph.wen@gmail.com>
2014-07-23 21:10:06 +0800

    fix typo

--- app/models.py
+++ app/models.py
@@ -74,7 +74,7 @@ class User(UserMixin, db.Model):
         if request.is_secure:
             url = 'https://secure.gravatar.com/avatar'
         else:
-            url = 'http://www/gravatar.com/avatar'
+            url = 'http://www.gravatar.com/avatar'
         hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url, hash=hash, size=size, default=default, rating=rating
