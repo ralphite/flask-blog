bb3d4bfb4a6327ab090b4197abfa06d4b91578f5
Yadong Wen <ralph.wen@gmail.com>
2014-07-25 23:24:03 +0800

    add following posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, session, redirect, url_for, current_app, abort, flash, request
+from flask import render_template, session, redirect, url_for, current_app, \
+    abort, flash, request, make_response
 
 from flask_login import login_required, current_user
 
@@ -18,12 +19,35 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    show_followed = False
+    if current_user.is_authenticated():
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASK_BLOG_POSTS_PER_PAGE'],
         error_out=False
     )
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts, pagination=pagination)
+
+
+@main.route('/post/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+
+@main.route('/post/following')
+@login_required
+def show_following():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+
 
 @main.route('/user/<username>')
 def user(username):
