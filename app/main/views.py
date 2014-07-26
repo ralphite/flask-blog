50e56996399c5a86e120b020a2b9d42e1b6ee402
Ralph Wen <ralph.wen@gmail.com>
2014-07-26 20:17:14 +0800

    fix bug in moderate button

--- app/main/views.py
+++ app/main/views.py
@@ -226,7 +226,7 @@ def moderate_enable(id):
     return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
 
 
-@main.route('/moderate/disable')
+@main.route('/moderate/disable<int:id>')
 @login_required
 @permission_required(Permission.MODERATE_COMMENTS)
 def moderate_disable(id):
