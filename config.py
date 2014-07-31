87a0559ddf23419b8a711876cb5bacc32c1afe91
Ralph Wen <ralph.wen@gmail.com>
2014-07-31 12:18:40 +0800

    add SMTPHander to send error with email

--- config.py
+++ config.py
@@ -42,6 +42,30 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') \
         or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        #email errors to the administrator
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+        if getattr(cls, 'MAIL_USER_TLS', None):
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASK_BLOG_MAIL_SENDER,
+            toaddrs=[cls.FLASK_BLOG_ADMIN],
+            subject=cls.FLASK_BLOG_MAIL_SUBJECT_PREFIX + 'Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 
 config = {
     'development': DevelopmentConfig,
